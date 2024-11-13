855c9437aa85d564479e3b35390257862b9a4e3c
rodrigomha <rodrigomha@gmail.com>
2024-11-12 16:05:16 -0800

    update API for models

--- src/capital/capital_models.jl
+++ src/capital/capital_models.jl
@@ -2,4 +2,6 @@ abstract type CapitalCostModel end
 
 struct DiscountedCashFlow <: CapitalCostModel
     discount_rate::Float64
+    base_year::Dates.Year
+    investment_years::Vector{NTuple{2, Dates.Date}}
 end
